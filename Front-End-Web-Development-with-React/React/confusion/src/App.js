5a44890c9a5226daadd5535cac49c0bf5a7f3e20
Daniel Terra Gomes <danielterra24@hotmail.com>
2020-08-22 01:46:55 -0300

    menu

--- Front-End-Web-Development-with-React/React/confusion/src/App.js
+++ Front-End-Web-Development-with-React/React/confusion/src/App.js
@@ -1,10 +1,12 @@
 
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <Navbar dark color="primary">
@@ -12,9 +14,11 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
-  }
+
+           }
 }
 
 export default App;
